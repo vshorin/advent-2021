7cf42e7f49fc4722023ecb638a30b31d92202fc3
vshorin <vladimirs.sorins@hotmail.com>
2021-12-16 08:28:55 +0200

    finish day 12

--- days/day12.go
+++ days/day12.go
@@ -3,6 +3,7 @@ package days
 import (
 	"bytes"
 	"log"
+	"strings"
 )
 
 func Day12() {
@@ -11,110 +12,165 @@ func Day12() {
 	} else {
 		log.Printf("day12_2 result = %d", res)
 	}
-	// if res, err := day12_2(); err != nil {
-	// 	log.Fatalf("crash in day12_2: %v", err)
-	// } else {
-	// 	log.Printf("day12_2 result = %d", res)
-	// }
+	if res, err := day12_2(); err != nil {
+		log.Fatalf("crash in day12_2: %v", err)
+	} else {
+		log.Printf("day12_2 result = %d", res)
+	}
 }
 
-type cave struct {
-	name     string
-	parents  []*cave
-	children []*cave
-}
-
-var startCave = cave{
-	name:     "start",
-	children: []*cave{},
-}
-var endCave = cave{
-	name:    "end",
-	parents: []*cave{},
-}
-
-func day12_1() (int, error) {
+func readCaves() (caves map[string][]string, err error) {
 	dash := []byte{byte('-')}
+	caves = map[string][]string{
+		"start": make([]string, 0),
+	}
 
-	var caves []*cave
-	err := ReadLines("./days/inputs/day12_x.txt", func(b []byte) error {
+	err = ReadLines("./days/inputs/day12_1.txt", func(b []byte) error {
 		spl := bytes.Split(b, dash)
 		str0 := string(spl[0])
 		str1 := string(spl[1])
-		newCave0 := addCave(caves, str0, nil)
-		newCave1 := addCave(caves, str1, nil)
-		caveRelationship(newCave0, newCave1)
+		addCave(str0, caves)
+		addCave(str1, caves)
+		caveRelationship(str0, str1, caves)
 		return nil
 	})
-	log.Printf("caves: %v", caves)
-
-	return 0, err
+	if err != nil {
+		return
+	}
+	log.Print("caves:")
+	for k, v := range caves {
+		log.Printf("%s: relations = %v", k, v)
+	}
+	return
 }
 
-func addCave(caves []*cave, name string, existing *cave) *cave {
-	for _, c := range caves {
-		if c.name == name {
-			return c
+func day12_1() (int, error) {
+	caveTravels = make([][]string, 0)
+
+	caves, err := readCaves()
+	if err != nil {
+		return 0, err
+	}
+	caveTravel("start", caves, make([]string, 0))
+	log.Print("travels:")
+	for _, v := range caveTravels {
+		log.Printf("\t%v", v)
+	}
+
+	return len(caveTravels), err
+}
+
+func day12_2() (int, error) {
+	caveTravels = make([][]string, 0)
+
+	caves, err := readCaves()
+	if err != nil {
+		return 0, err
+	}
+	startCave := travelTracker{
+		travel: make([]string, 0),
+		dupe:   false,
+	}
+	caveTravelTracked("start", caves, startCave)
+	// log.Print("travels:")
+	// for _, v := range caveTravels {
+	// 	log.Printf("\t%v", strings.Join(v, ","))
+	// }
+
+	return len(caveTravels), err
+}
+
+// var knownTravels = make(map[string][][]string)
+var caveTravels [][]string
+
+func caveTravel(start string, caves map[string][]string, travel []string) {
+
+	travel = append(travel, start)
+	for _, v := range caves[start] {
+		if v == "end" {
+			caveTravels = append(caveTravels, append(travel, "end"))
+			continue
+		}
+		if byte(v[0]) >= 97 {
+			if start != "start" && travel[len(travel)-2] == v {
+				continue
+			}
+			if stringSliceContains(travel, v) {
+				continue
+			}
+		}
+		newTravel := make([]string, len(travel))
+		copy(newTravel, travel)
+		caveTravel(v, caves, newTravel)
+	}
+}
+
+type travelTracker struct {
+	travel []string
+	dupe   bool
+}
+
+func checkUniqueSmallCave(c string, travel *travelTracker) bool {
+	count := 0
+	for _, v := range travel.travel {
+		if v == c {
+			count++
 		}
 	}
-	if existing != nil {
-		caves = append(caves, existing)
-		return existing
+	if count == 0 {
+		return true
 	}
-	var newCave *cave
-	if name == "start" {
-		newCave = &startCave
-	} else if name == "end" {
-		newCave = &endCave
-	} else {
-		newCave = &cave{
-			name:     name,
-			parents:  make([]*cave, 0),
-			children: make([]*cave, 0),
+	if travel.dupe {
+		return false
+	}
+	if count < 2 {
+		travel.dupe = true
+		return true
+	}
+	return false
+}
+
+func caveTravelTracked(start string, caves map[string][]string, travel travelTracker) {
+	// start,A,c,A,b,end
+	// start,A,c,A,b,d,b,A,end
+	travel.travel = append(travel.travel, start)
+	if strings.Join(travel.travel, ",") == "start,A,c,A,b" {
+		log.Println("aa")
+	}
+	for _, v := range caves[start] {
+		if v == "end" {
+			caveTravels = append(caveTravels, append(travel.travel, "end"))
+			continue
 		}
-	}
-	caves = append(caves, newCave)
-	return newCave
-}
-
-func caveRelationship(thisCave *cave, otherCave *cave) {
-	// only start and capital letter cave can have child caves
-	if thisCave.name == "start" || byte(thisCave.name[0]) < 97 {
-		addCave(thisCave.children, otherCave.name, otherCave)
-	}
-	if otherCave.name == "start" || byte(otherCave.name[0]) < 97 {
-		addCave(otherCave.children, thisCave.name, thisCave)
-	}
-	// all caves except start can have parent caves
-	if thisCave.name != "start" {
-		addCave(thisCave.parents, otherCave.name, otherCave)
-	}
-	if otherCave.name != "start" {
-		addCave(otherCave.parents, thisCave.name, thisCave)
+		newTravel := make([]string, len(travel.travel))
+		copy(newTravel, travel.travel)
+		tt := travelTracker{
+			dupe:   travel.dupe,
+			travel: newTravel,
+		}
+		if byte(v[0]) >= 97 {
+			if start != "start" && !checkUniqueSmallCave(v, &tt) {
+				continue
+			}
+			//if stringSliceContains(travel.travel, v) {
+			//	continue
+			//}
+		}
+		caveTravelTracked(v, caves, tt)
 	}
 }
 
-// func day12_2() (int, error) {
-// 	octopuses := make(map[string]*octopus)
-// 	nr := 0
-// 	err := ReadLines("./days/inputs/day12_1.txt", func(b []byte) error {
-// 		for i, v := range b {
-// 			octopuses[fmt.Sprintf("%d:%d", nr, i)] = &octopus{charge: byteToInt(v)}
-// 		}
-// 		nr++
-// 		return nil
-// 	})
-// 	log.Print("octopuses start")
-// 	printOctopus(octopuses)
-// 	res := 0
-// 	for {
-// 		blinks := octoBlink(octopuses)
-// 		res++
-// 		if blinks == 100 {
-// 			break
-// 		}
-// 	}
-// 	log.Print("octopuses end")
-// 	printOctopus(octopuses)
-// 	return res, err
-// }
+func addCave(name string, caves map[string][]string) {
+	if _, ok := caves[name]; !ok {
+		caves[name] = make([]string, 0)
+	}
+}
+
+func caveRelationship(thisCave string, otherCave string, caves map[string][]string) {
+	if otherCave != "start" {
+		caves[thisCave] = append(caves[thisCave], otherCave)
+	}
+	if thisCave != "start" {
+		caves[otherCave] = append(caves[otherCave], thisCave)
+	}
+}
